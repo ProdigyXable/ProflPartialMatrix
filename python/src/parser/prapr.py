4b99b72bff89a50775b4c95b92f847630b142272
Mengshi Zhang <mengshi.zhang@utexas.edu>
2021-02-19 13:29:47 -0800

    add partial matrix support

--- python/src/parser/prapr.py
+++ python/src/parser/prapr.py
@@ -1,17 +1,18 @@
 from pprint import pprint
+from parser.base import ParserBase
 import os
 import json
 
 
-class PraprParser:
-    def __init__(self, root_dir, output_dir):
+class PraprParser(ParserBase):
+    def __init__(self, root_dir, output_dir, matrix_type="full"):
         self._root_dir = root_dir
-        self._output_dir = output_dir
+        self._output_dir = os.path.join(output_dir, matrix_type)
         self.project_list = ["Chart", "Lang", "Math", "Time"]
         self._tool_name = "prapr"
+        self._matrix_type = matrix_type
 
-        if not os.path.exists(self._output_dir):
-            os.makedirs(self._output_dir)
+        os.makedirs(self._output_dir, exist_ok=True)
 
 
     def _process_queue(self, line_queue):
@@ -30,11 +31,11 @@ class PraprParser:
                     methodDesc = i.replace("methodDesc=", "")
 
             return clazz + ":" + method + methodDesc
-        
+
         def parse_test(test_str):
             failed_test = test_str.split(" ")[1].split("(")[0]
             return failed_test
-        
+
         patch_result = {
             "patch": "",
             "failed_tests": set(),
@@ -52,24 +53,24 @@ class PraprParser:
 
 
     def _parse_subject_patch_results(self, subject_filename):
-        line_stack = []
+        line_queue = []
         patch_count = 0
         subject_patch_dict = {}
 
         with open(subject_filename) as file:
             for line in file:
-                if line.startswith("MutationDetails, ") and len(line_stack) > 0:
-                    patch_result = self._process_queue(line_stack)
+                if line.startswith("MutationDetails, ") and len(line_queue) > 0:
+                    patch_result = self._process_queue(line_queue)
                     patch_count += 1
                     subject_patch_dict[str(patch_count)] = patch_result
-                
-                line_stack.append(line)
 
-            patch_result = self._process_queue(line_stack)
-            if len(patch_result["failed_tests"]) > 0:
+                line_queue.append(line)
+
+            if len(line_queue) > 0:
+                patch_result = self._process_queue(line_queue)
                 patch_count += 1
                 subject_patch_dict[str(patch_count)] = patch_result
-        
+
         return subject_patch_dict
 
 
@@ -90,7 +91,7 @@ class PraprParser:
             )
         except:
             print(os.path.join(patch_path, patch_filename))
-        
+
         org_failing_tests = self._parse_original_failing_tests(
             os.path.join(test_path, patch_filename)
         )
@@ -99,43 +100,61 @@ class PraprParser:
 
         for patch_id, patch_content in subject_patch_dict.items():
             patch_failing_tests = patch_content["failed_tests"]
-            pf = patch_failing_tests - org_failing_tests
-            fp = org_failing_tests - patch_failing_tests
-            ff = patch_failing_tests & org_failing_tests
+            pf = list(patch_failing_tests - org_failing_tests)
+            fp = list(org_failing_tests - patch_failing_tests)
+            ff = list(patch_failing_tests & org_failing_tests)
 
-            ff_len = len(ff)
-            fp_len = len(fp)
-            pf_len = len(pf)
-            patch_category = ""
+            if self._matrix_type == "partial":
+                pf, fp, ff = self._get_partial_matrix(pf, fp, ff)
 
-            if fp_len > 0 and pf_len == 0 and ff_len == 0:
-                patch_category = "PatchCategory.CleanFixFull"
-
-            if fp_len > 0 and pf_len == 0 and ff_len > 0:
-                patch_category = "PatchCategory.CleanFixPartial"
-
-            if fp_len > 0 and pf_len > 0 and ff_len == 0:
-                patch_category = "PatchCategory.NoisyFixFull"
-
-            if fp_len > 0 and pf_len > 0 and ff_len > 0:
-                patch_category = "PatchCategory.NoisyFixPartial"
-
-            if fp_len == 0 and pf_len == 0 and ff_len > 0:
-                patch_category = "PatchCategory.NoneFix"
-
-            if fp_len == 0 and pf_len > 0 and ff_len > 0:
-                patch_category = "PatchCategory.NegFix"
+            patch_category = self._get_patch_category(len(fp), len(pf), len(ff))
 
             merged_result_dict[patch_id] = {
                 "patch_category": patch_category,
-                "ff_test": list(ff),
-                "fp_test": list(fp),
-                "pf_test": list(pf),
+                "ff_test": ff,
+                "fp_test": fp,
+                "pf_test": pf,
                 "pp_test": [],
                 "patch": [patch_content["patch"]]
             }
         
         return merged_result_dict
+
+
+    def _get_partial_matrix(self, pf_list, fp_list, ff_list):
+        org_failed_test_list = fp_list + ff_list
+        org_passed_test_list = pf_list
+
+        test_category_map = {}
+        test_category_map.update({
+            test_i: "pf" for test_i in pf_list
+        })
+        test_category_map.update({
+            test_i: "fp" for test_i in fp_list
+        })
+        test_category_map.update({
+            test_i: "ff" for test_i in ff_list
+        })
+
+        updated_pf_list = []
+        updated_fp_list = []
+        updated_ff_list = []
+
+        sorted_test_list = sorted(org_failed_test_list) + sorted(org_passed_test_list)
+        for test_i in sorted_test_list:
+            if test_category_map[test_i] == "ff":
+                updated_ff_list.append(test_i)
+
+            if test_category_map[test_i] == "pf":
+                updated_pf_list.append(test_i)
+
+            if test_category_map[test_i] == "fp":
+                updated_fp_list.append(test_i)
+
+            if test_category_map[test_i] in ["ff", "pf"]:
+                break
+
+        return updated_pf_list, updated_fp_list, updated_ff_list
 
 
     def parse_all_subjects(self):
@@ -159,10 +178,16 @@ class PraprParser:
             json.dump(repair_result_dict, json_file, indent=4)
 
 
-
 if __name__ == "__main__":
     data_root_dir = os.path.abspath("../../data/prapr")
     output_dir = os.path.abspath("../../parsed_data")
-    pp = PraprParser(data_root_dir, output_dir)
-    pp.parse_all_subjects()
+    # pp = PraprParser(data_root_dir, output_dir)
+    # pp.parse_all_subjects()
+
+    # patch_path = "/filesystem/patch_ranking/ProflPartialMatrix/python/data/prapr/Patches/Chart/mutation-test"
+    # test_path = "/filesystem/patch_ranking/ProflPartialMatrix/python/data/prapr/FailingTests/Chart"
+    # patch_file_i = "11.txt"
+    # pp = PraprParser(data_root_dir, output_dir)
+    # x = pp._merge_patch_and_test_result(patch_path, test_path, patch_file_i)
+    # pprint(x)
 
