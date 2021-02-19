16a573b76bbf840caadf61fa5a991de7f43ab60f
Mengshi Zhang <mengshi.zhang@utexas.edu>
2021-02-19 00:14:18 -0800

    udpate

--- python/src/run_parser.py
+++ python/src/run_parser.py
@@ -11,43 +11,36 @@ output_dir = os.path.abspath("../parsed_data")
 astor_data_root_dir = os.path.abspath("../data/astor/output_astor")
 for tool in ["cardumen", "jGenProg", "jKali", "jmutrepair"]:
     print("Processing {}".format(tool))
-    ap = AstorParser(astor_data_root_dir, tool, output_dir)
+    ap = AstorParser(astor_data_root_dir, tool, output_dir, matrix_type="full")
     ap.runAll()
+    ap = AstorParser(astor_data_root_dir, tool, output_dir, matrix_type="partial")
+    ap.runAll()
+
 
 # ArjaParser
 arja_data_root_dir = os.path.abspath("../data/arja")
 for tool in ["arja", "genprog", "kali", "rsrepair"]:
     print("Processing {}".format(tool))
-    ap = ArjaParser(arja_data_root_dir, tool, output_dir)
+    ap = ArjaParser(arja_data_root_dir, tool, output_dir, matrix_type="full")
+    ap.runAll()
+    ap = ArjaParser(arja_data_root_dir, tool, output_dir, matrix_type="partial")
     ap.runAll()
 
+
 # AvatarParser
-avatar_data_root_dir = os.path.abspath("../data/avatar")
-tool = "avatar"
-print("Processing {}".format(tool))
-ap = AvatarParser(avatar_data_root_dir, tool, output_dir)
-ap.runAll()
+for tool in ["avatar", "kpar", "tbar", "fixminer"]:
+    avatar_data_root_dir = os.path.join(
+        os.path.abspath("../data"), tool
+    )
+    print("Processing {}".format(tool))
+    ap = AvatarParser(avatar_data_root_dir, tool, output_dir, matrix_type="full")
+    ap.runAll()
+    ap = AvatarParser(avatar_data_root_dir, tool, output_dir, matrix_type="partial")
+    ap.runAll()
 
-avatar_data_root_dir = os.path.abspath("../data/kPar")
-tool = "kpar"
-print("Processing {}".format(tool))
-ap = AvatarParser(avatar_data_root_dir, tool, output_dir)
-ap.runAll()
 
-avatar_data_root_dir = os.path.abspath("../data/tbar")
-tool = "tbar"
-print("Processing {}".format(tool))
-ap = AvatarParser(avatar_data_root_dir, tool, output_dir)
-ap.runAll()
-
-avatar_data_root_dir = os.path.abspath("../data/fixminer")
-tool = "fixminer"
-print("Processing {}".format(tool))
-ap = AvatarParser(avatar_data_root_dir, tool, output_dir)
-ap.runAll()
-
-# PraprParser
-prapr_data_root_dir = os.path.abspath("../data/prapr")
-print("Processing {}".format("prapr"))
-pp = PraprParser(prapr_data_root_dir, output_dir)
-pp.runAll()
+# # PraprParser
+# prapr_data_root_dir = os.path.abspath("../data/prapr")
+# print("Processing {}".format("prapr"))
+# pp = PraprParser(prapr_data_root_dir, output_dir)
+# pp.runAll()
