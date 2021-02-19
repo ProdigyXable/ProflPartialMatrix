7f91da1631235800e9e5c8fda92f2604fbe8b0b9
Mengshi Zhang <mengshi.zhang@utexas.edu>
2021-02-19 14:19:23 -0800

    move formulas to utils

---
+++ python/src/utils.py
@@ -0,0 +1,94 @@
+import math
+
+
+def compute_score(tP, fP, tN, fN, stats):
+    # this is moved from ProflPartialMatrix/src/main/java/com/mycompany/patchstatistics/Stats.java
+    tP = float(tP)
+    fP = float(fP)
+    tN = float(tN)
+    fN = float(fN)
+
+    predicted_positive = tP + fP
+    predicted_negative = tN + fN
+
+    actual_positive = tP + fN
+    actual_negative = fP + tN
+
+    total = tP + tN + fP + fN
+
+    # SBFL
+    ef = tP
+    ep = tN
+    nf = fP
+    np = fN
+
+    if stats == "prevalence":
+        return (actual_positive) / (total)
+
+    elif stats == "accuracy":
+        return (tP + tN) / (total)
+
+    elif stats == "recall":
+        return tP / actual_positive
+
+    elif stats == "missRate":
+        return fN / actual_positive
+
+    elif stats == "specificity":
+        return tN / actual_negative
+
+    elif stats == "fallOut":
+        return fP / actual_negative
+
+    elif stats == "precision":
+        return tP / predicted_positive
+
+    elif stats == "falseDiscoveryRate":
+        return fP / predicted_positive
+
+    elif stats == "negativePredictiveRate":
+        return tN / predicted_negative
+
+    elif stats == "falseOmissionRate":
+        return fN / predicted_negative
+
+    elif stats == "positiveLikelihood":
+        return compute_score(tP, fP, tN, fN, "recall") / compute_score(tP, fP, tN, fN, "fallOut")
+
+    elif stats == "negativeLikelihood":
+        return compute_score(tP, fP, tN, fN, "missRate") / compute_score(tP, fP, tN, fN, "specificity")
+
+    elif stats == "diagnosticOdds":
+        return compute_score(tP, fP, tN, fN, "positiveLikelihood") / compute_score(tP, fP, tN, fN, "negativeLikelihood")
+
+    elif stats == "fScore":
+        precision = compute_score(tP, fP, tN, fN, "precision")
+        recall = compute_score(tP, fP, tN, fN, "recall")
+        return (2 * (precision * recall)) / (precision + recall)
+
+    elif stats == "threatScore":
+        return tP / (tP + fN + fP)
+
+    elif stats == "Tarantula":
+        return (ef / (ef + nf)) / ((ef / (ef + nf)) + (ep / (ep + np)))
+
+    elif stats == "Ochiai":
+        return ef / math.sqrt((ef + ep) * (ef + nf))
+
+    elif stats == "Ochiai2":
+        return ef * np / math.sqrt((ef + ep) * (nf + np) * (ef + np) * (nf + ep))
+
+    elif stats == "Op2":
+        return ef - ep / (ep + np + 1)
+
+    elif stats == "SBI":
+        return 1 - ep / (ep + ef)
+
+    elif stats == "Jaccard":
+        return ef / (ef + ep + nf)
+
+    elif stats == "Kulczynski":
+        return ef / (nf + ep)
+
+    elif stats == "Dstar2":
+        return ef * ef / (ep + nf)
